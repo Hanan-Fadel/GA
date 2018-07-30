e15f4a11e9cca8875c6f554157bbe8f829986d2f
Si Chen@digitalocean <quake0day@gmail.com>
2018-07-30 04:24:04 +0000

    add deploy script

---
+++ server_deploy.js
@@ -0,0 +1,40 @@
+const next = require('next');
+const express = require('express');
+const routes = require('next-routes')();
+const fs = require('fs');
+const https = require('https');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 80;
+const app = next({ dev });
+const handler = routes.getRequestHandler(app);
+
+routes.add('/talks/:talkKey', 'talks');
+
+var sslOption ={
+	key: fs.readFileSync('./elianaai.key'),
+	cert: fs.readFileSync('./4fa581949f17aaa.crt')
+}
+
+app.prepare().then(() =>{
+const server = express();
+var http = require('http');
+server.use(handler);
+http.createServer(server).listen(80);
+https.createServer(sslOption, server).listen(443);
+});
+/*
+server.get('/', function(req, res){
+	res.status(200).send("This is aaa https");
+});
+*/
+
+/*
+app.prepare().then(() => {
+  const server = express();
+  server.use(handler);
+  server.listen(3000);
+});
+
+*/
+
